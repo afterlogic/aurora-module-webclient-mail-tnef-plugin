40317e3bdaa1d35a945f85a366c2fe2d338401ce
Vasil Sokolov <vasil@afterlogic.com>
2023-04-19 21:42:44 +0200

    Refactored getting module settings (part2)

--- Module.php
+++ Module.php
@@ -14,6 +14,8 @@ namespace Aurora\Modules\MailTnefWebclientPlugin;
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2023, Afterlogic Corp.
  *
+ * @property Settings $oModuleSettings
+ *
  * @package Modules
  */
 class Module extends \Aurora\System\Module\AbstractModule
